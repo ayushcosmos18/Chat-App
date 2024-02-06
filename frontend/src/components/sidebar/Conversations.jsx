94c150dd0a23957502b81226cdf8a5252afd334e
Ayush <ayushcosmos456@gmail.com>
2024-02-07 03:51:27 +0530

    2nd Commit Frontend UI

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,18 @@
+import Conversation from "./Conversation"
+
+const Conversations = () => {
+  return (
+    <div className="py-3 flex flex-col overflow-auto">
+
+
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+    </div>
+  )
+}
+
+export default Conversations
