f598dfe4b12bbc57d7ef300ce6418eb716016043
Ayush <ayushcosmos456@gmail.com>
2024-02-12 03:49:01 +0530

    ulululu

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,10 +2,15 @@ import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages"
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message"
+import useListenMessages from "../../hooks/useListenMessages";
+import useConversation from "../../zustand/useConversations";
 
 const Messages = () => {
 
+  const{selectedConversation,setSelectedConversation}=useConversation()
+
   const {messages,loading}=useGetMessages();
+  useListenMessages();
   const lastMessageRef=useRef();
 
   useEffect(()=>{
@@ -17,11 +22,22 @@ const Messages = () => {
 
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {!loading&& messages.length>0 && messages.map((message)=>(
+      {/* {!loading&& messages.length>0 && messages.map((message)=>(
         <div key={message._id} ref={lastMessageRef} >
         <Message message={message} />
       </div>
-      ))}
+      ))} */}
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => {
+          if (message.receiverId === selectedConversation._id || message.senderId === selectedConversation._id) { //Add this check to only render message if it belongs to current conversation
+            return (
+              <div key={message._id} ref={lastMessageRef}>
+                <Message message={message} />
+              </div>
+            );
+          }
+})}
 
        {loading&& [...Array(3)].map((_,idx)=><MessageSkeleton key={idx}/>)}
        {!loading && messages.length===0 && (
